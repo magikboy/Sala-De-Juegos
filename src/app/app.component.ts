94f1c683b27608fd99cb5f7f7b11bea4719b84a7
massimo bosco <massimo.bosco26@gmail.com>
2024-09-12 11:59:31 -0300

    APP responsive en su totalidad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from './auth.service';
@@ -13,17 +13,34 @@ import { AuthService } from './auth.service';
 export class AppComponent {
   title = 'salaJuegos';
   isAuthenticated: boolean = false;
+  menuOpen: boolean = false;
+  isDesktop: boolean = false;
 
   constructor(private authService: AuthService, private router: Router) {
     // Escuchar el estado de autenticación
     this.authService.user$.subscribe((user) => {
       this.isAuthenticated = !!user;
     });
+    this.checkWindowSize();
   }
 
   // Método para cerrar sesión
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  // Método para alternar el menú en móviles
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  // Verificar si está en pantalla grande o móvil
+  @HostListener('window:resize', ['$event'])
+  checkWindowSize() {
+    this.isDesktop = window.innerWidth >= 1024;
+    if (this.isDesktop) {
+      this.menuOpen = false;
+    }
   }
 }
